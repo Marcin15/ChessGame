87045d5117b3ce260d01ede26cf1898d0c28f7b0
Marcin15 <mkoperczuk1@wp.pl>
2021-05-21 17:31:33 +0200

    Added RelayCommand.cs

---
+++ ChessGame.Core/Helpers/RelayCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace ChessGame
+{
+    public class RelayCommand : ICommand
+    {
+        private Action<object> mAction;
+        public event EventHandler CanExecuteChanged;
+
+        public RelayCommand(Action<object> action)
+        {
+            mAction = action;
+        }
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            mAction(parameter);
+        }
+    }
+}
