d1ac9b3239187310498dc415a7015a206cd677fa
Marcin15 <mkoperczuk1@wp.pl>
2021-05-21 17:32:22 +0200

    Added FigureIntereactionManager.cs

---
+++ ChessGame.Core/Managers/FigureInteractionManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace ChessGame.Core
+{
+    public class FigureInteractionManager : IManager
+    {
+        private IField mOldClickedFigure;
+        public void Container(IField field, ObservableCollection<IField> fieldsList)
+        {
+            AssignPreviousClickedFigure(field);
+            MoveFigure(field, fieldsList);
+        }
+        private void AssignPreviousClickedFigure(IField clickedField)
+        {
+            if (clickedField.CurrentFigure is Figure)
+            {
+                mOldClickedFigure = clickedField;
+            }
+            if (!clickedField.IsClicked)
+                mOldClickedFigure = null;
+        }
+        private void MoveFigure(IField field, ObservableCollection<IField> fieldsList)
+        {
+            if(mOldClickedFigure is not null && 
+                field.CurrentFigure is not Figure && 
+                mOldClickedFigure.CurrentFigure is Figure)
+            {
+                mOldClickedFigure.CurrentFigure.Move(mOldClickedFigure, field, fieldsList);
+                mOldClickedFigure = null;
+            }
+        }
+    }
+}
