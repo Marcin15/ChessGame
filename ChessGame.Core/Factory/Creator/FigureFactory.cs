532e189ecc8863d60ac8f57c7cf54cf82ae1d146
Marcin15 <mkoperczuk1@wp.pl>
2021-05-21 17:27:26 +0200

    Created Factory pattern

---
+++ ChessGame.Core/Factory/Creator/FigureFactory.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace ChessGame.Core
+{
+    class FigureFactory : IFigureCreator
+    {
+        public void Create(List<IField> fields)
+        {
+            IFigureFactory mFactory = null;
+            Figure figure = null;
+            FiguresStartUpLocation[] startUpLocations = FigureStartUpLocationModel.startUpLocations;
+
+            for (int i = 0; i < startUpLocations.Length; i++)
+            {
+                switch (startUpLocations[i].TypeOfFigure)
+                {
+                    case "Rook":
+                        mFactory = new RookFactory(fields.Where(x => x.RowIndex == startUpLocations[i].Row && x.ColumnIndex == startUpLocations[i].Column).FirstOrDefault(), startUpLocations[i].Player);
+                        figure = mFactory.GetFigure();
+                        break;
+                    case "Knight":
+                        mFactory = new KnightFactory(fields.Where(x => x.RowIndex == startUpLocations[i].Row && x.ColumnIndex == startUpLocations[i].Column).FirstOrDefault(), startUpLocations[i].Player);
+                        figure = mFactory.GetFigure();
+                        break;
+                    case "Bishop":
+                        mFactory = new BishopFactory(fields.Where(x => x.RowIndex == startUpLocations[i].Row && x.ColumnIndex == startUpLocations[i].Column).FirstOrDefault(), startUpLocations[i].Player);
+                        figure = mFactory.GetFigure();
+                        break;
+                    case "Queen":
+                        mFactory = new QueenFactory(fields.Where(x => x.RowIndex == startUpLocations[i].Row && x.ColumnIndex == startUpLocations[i].Column).FirstOrDefault(), startUpLocations[i].Player);
+                        figure = mFactory.GetFigure();
+                        break;
+                    case "King":
+                        mFactory = new KingFactory(fields.Where(x => x.RowIndex == startUpLocations[i].Row && x.ColumnIndex == startUpLocations[i].Column).FirstOrDefault(), startUpLocations[i].Player);
+                        figure = mFactory.GetFigure();
+                        break;
+                    case "Pawn":
+                        mFactory = new PawnFactory(fields.Where(x => x.RowIndex == startUpLocations[i].Row && x.ColumnIndex == startUpLocations[i].Column).FirstOrDefault(), startUpLocations[i].Player);
+                        figure = mFactory.GetFigure();
+                        break;
+                    default:
+                        throw new Exception();
+                }
+            }
+        }
+    }
+}
---
+++ ChessGame.Core/Factory/KnightFactory.cs
@@ -0,0 +1,18 @@
+﻿namespace ChessGame.Core
+{
+    public class KnightFactory : IFigureFactory
+    {
+        private IField mField;
+        private Player mPlayer;
+
+        public KnightFactory(IField field, Player player)
+        {
+            mField = field;
+            mPlayer = player;
+        }
+        public Figure GetFigure()
+        {
+            return new Knight(mPlayer, mField);
+        }
+    }
+}
