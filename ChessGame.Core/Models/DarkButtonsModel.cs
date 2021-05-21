f0c0f7deb3e06fefcded098c9b57e1a7f351fdf4
Marcin15 <mkoperczuk1@wp.pl>
2021-05-21 17:32:38 +0200

    Added Models

---
+++ ChessGame.Core/Models/DarkButtonsModel.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+
+namespace ChessGame.Core
+{
+    public class DarkButtonsModel
+    {
+        public List<DarkButtonsViewModel> DarkButtonsList = new List<DarkButtonsViewModel>();
+
+        private int rowIndex = 0;
+        private int columnIndex = 0;
+        public DarkButtonsModel()
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        if (j % 2 == 1)
+                        {
+                            columnIndex = j;
+                            rowIndex = i;
+                        }
+                        else
+                            continue;
+                    }
+                    else if (i % 2 == 1)
+                    {
+                        if (j % 2 == 0)
+                        {
+                            columnIndex = j;
+                            rowIndex = i;
+                        }
+                        else
+                            continue;
+                    }
+
+                    DarkButtonsList.Add(new DarkButtonsViewModel
+                    {
+                        RowIndex = rowIndex,
+                        ColumnIndex = columnIndex
+                    });
+                }
+            }
+        }
+    }
+}
+
