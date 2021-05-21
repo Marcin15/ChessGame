f907f48da64c6339777c0fa1bf63689811f73117
Marcin15 <mkoperczuk1@wp.pl>
2021-05-21 17:29:34 +0200

    Added FiguresStartUpLocation.cs

---
+++ ChessGame.Core/FiguresStartUpLocation.cs
@@ -0,0 +1,17 @@
+﻿namespace ChessGame.Core
+{
+    public class FiguresStartUpLocation
+    {
+        public int Row { get; private set; }
+        public int Column { get; private set; }
+        public string TypeOfFigure { get; set; }
+        public Player Player { get; private set; }
+        public FiguresStartUpLocation(int row, int column, Player player, string typeOfFigure)
+        {
+            this.Row = row;
+            this.Column = column;
+            this.Player = player;
+            this.TypeOfFigure = typeOfFigure;
+        }
+    }
+}
