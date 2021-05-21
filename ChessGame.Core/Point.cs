5bc7d49d653c3c474e530d013bb84dd6d805b518
Marcin15 <mkoperczuk1@wp.pl>
2021-05-21 17:29:48 +0200

    Added Point.cs

---
+++ ChessGame.Core/Point.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChessGame.Core
+{
+    public class Point
+    {
+        public int RowIndex { get; private set; }
+        public int ColumnIndex { get; private set; }
+        public Point(int rowIndex, int columnIndex)
+        {
+            this.RowIndex = rowIndex;
+            this.ColumnIndex = columnIndex;
+        }
+    }
+}
